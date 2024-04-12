8f713e6e96bba824b815caa81c5cc4049f3d2919
Daniel Munro <danbmunro@gmail.com>
2024-04-12 14:57:53 -0700

    Add NAcc3_PL3_VTA dataset

--- scripts/genotypes_rn7.sh
+++ scripts/genotypes_rn7.sh
@@ -137,6 +137,25 @@ bcftools norm geno_rn7/intermediate/Adipose_Liver.2.vcf.gz \
     -O z -o geno_rn7/intermediate/Adipose_Liver.vcf.gz
 tabix -f geno_rn7/intermediate/Adipose_Liver.vcf.gz
 
+### NAcc3_PL3_VTA ###
+echo '*** Preparing NAcc3_PL3_VTA genotypes...'
+plink2 --vcf $ROUND10_2_VCF \
+    --set-all-var-ids 'chr@:#' \
+    --fa ref_rn7/Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa \
+    --ref-from-fa force \
+    --keep geno_rn7/intermediate/NAcc3_PL3_VTA_ids.txt \
+    --recode vcf \
+    --out geno_rn7/intermediate/NAcc3_PL3_VTA.1
+bgzip geno_rn7/intermediate/NAcc3_PL3_VTA.1.vcf
+bcftools norm geno_rn7/intermediate/NAcc3_PL3_VTA.1.vcf.gz \
+    --rm-dup snps \
+    --check-ref x \
+    --fasta-ref ref_rn7/Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa \
+    -Ou | bcftools annotate \
+    -x ^INFO/AC,INFO/AN,^FORMAT/GT \
+    -O z -o geno_rn7/intermediate/NAcc3_PL3_VTA.vcf.gz
+tabix -f geno_rn7/intermediate/NAcc3_PL3_VTA.vcf.gz
+
 ### Final processing ###
 for DSET in IL_LHb_NAcc_OFC_PL Eye Adipose_Liver Brain BLA_NAcc2_PL2; do
 # for DSET in Adipose_Liver; do
